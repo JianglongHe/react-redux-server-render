22ac24736505236e2c61b8c9b6ffcbe6bd30e3f6
634584745@qq.com <a2262172>
2017-03-18 16:46:28 +0800

    增加校验

--- src/shared/containers/Home.js
+++ src/shared/containers/Home.js
@@ -1,26 +1,35 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import  { getProfile }  from '../actions/home'
 import { selectHome } from '../reducers/home'
 const mapDispatchToProps = {
-    profile
+    getProfile
 }
 
 const mapStateToProps = (state) => ({
     homeData: state
 })
 
-class About extends React.Component {
-  render() {
-      console.log(this.props.homeData)
-      return (
+class About extends Component {
+    render() {
+        const {
+            getProfile = () => {},
+            homeData = {}
+        } = this.props;
+
+        return (
           <div>
               <h2>Home</h2>
-              <button onClick={this.props.getProfile}>click</button>
-              <p>ss</p>
+              <button onClick={getProfile}>click</button>
+              <p>{homeData.name}</p>
           </div>
         )
-  }
+    }
+}
+
+About.propTypes = {
+    getProfile: PropTypes.func.isRequired,
+    homeData: PropTypes.object.isRequired
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(About)
--- src/shared/reducers/home.js
+++ src/shared/reducers/home.js
@@ -6,9 +6,7 @@ const homeData = {
 export default function home(state = {}, action) {
     switch (action.type) {
         case REQUEST_POSTS:
-            console.log(action.payload)
             const data = Object.assign(homeData, { name: action.payload })
-            console.log(data)
             return Object.assign(state, data)
         default:
             return state
